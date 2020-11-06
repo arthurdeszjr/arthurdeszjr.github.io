a2c6b0d56ec69795d4aee3fce66160ea43f5f695
arthurdeszjr <arthurdsjrj@gmail.com>
2020-11-06 17:01:04 -0300

    same shit

--- modCounter.js
+++ modCounter.js
@@ -10,3 +10,14 @@ function CountImages (t)
 	count += 1;
 	document.write(count + " / " + total);
 }
+
+var number = 0
+function CountBlocks (t)
+{
+	if (t != null)
+	{
+		total = t;
+	}
+	number += 1;
+	return number
+}
